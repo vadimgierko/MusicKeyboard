ca81494639fa25603545bce3d182950573890f9a
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-09-16 23:29:43 +0200

    can play melody 1.5 octave range + predefined chords in left hand

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const KEYS = ["q", "2", "w", "3", "e", "r", "5", "t", "6", "y", "7", "u", "i", "9", "o", "0", "p", "[", "=", "]"];
 const SOUND_ID = ["C", "Cis", "D", "Dis", "E", "F", "Fis", "G", "Gis", "A", "B", "H", "C2", "Cis2", "D2", "Dis2", "E2", "F2", "Fis2", "G2"];
 
-//const keys = document.querySelectorAll(".key");
-//console.log(keys);
-
 document.addEventListener("keydown", (e) => {
   if (e.repeat) return;
   const key = e.key;
-  console.log(key);
+  //console.log(key);
   const keyIndex = KEYS.indexOf(key);
-  console.log(keyIndex);
-  console.log(SOUND_ID[keyIndex]);
+  //console.log(keyIndex);
+  //console.log(SOUND_ID[keyIndex]);
   playSound(SOUND_ID[keyIndex])
 });
 
@@ -20,32 +17,39 @@ function playSound(soundId) {
   soundAudio.play();
 }
 
-// left hand => harmony / chords:
-  /*
+// harmony in left hand => predefined chords ready to play
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return;
+  const key = e.code;
+
   if (key === "KeyA") {
-    soundA.play();
-    soundC.play();
-    soundE.play();
+    document.getElementById("C").play();
+    document.getElementById("E").play();
+    document.getElementById("A").play();
   } else if (key === "KeyC") {
-    soundC.play();
-    soundE.play();
-    soundG.play();
+    document.getElementById("C").play();
+    document.getElementById("E").play();
+    document.getElementById("G").play();
+    document.getElementById("C2").play();
   } else if (key === "KeyF") {
-    soundA.play();
-    soundC.play();
-    soundF.play();
+    document.getElementById("F").play();
+    document.getElementById("A").play();
+    document.getElementById("C2").play();
+    document.getElementById("F2").play();
   } else if (key === "KeyG") {
-    soundB.play();
-    soundD.play();
-    soundG.play();
+    document.getElementById("G").play();
+    document.getElementById("H").play();
+    document.getElementById("D2").play();
+    document.getElementById("G2").play();
   } else if (key === "KeyD") {
-    soundA.play();
-    soundD.play();
-    soundF.play();
-  } else if (key === "KeyE") {
-    soundGis.play();
-    soundB.play();
-    soundD.play();
-    soundE.play();
+    document.getElementById("D").play();
+    document.getElementById("F").play();
+    document.getElementById("A").play();
+    document.getElementById("D2").play();
+  } else if (key === "KeyX") { // E7 CHORD
+    document.getElementById("E").play();
+    document.getElementById("Gis").play();
+    document.getElementById("H").play();
+    document.getElementById("D").play();
   }
-  */
+});
